bbd8ec5e00b2f9989d7f42425e1f47a348f90283
Johnny-Batata <johnny.arguello007@gmail.com>
2021-03-01 15:30:44 -0300

    atualizando

---
+++ exercises/bloco_4/dia_3/ex0001.js
@@ -0,0 +1,56 @@
+/*//ex do conteudo
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+
+for(let index = 0; index < fruits.length; index++) {
+    soma += fruits[index]
+}     
+    if (soma < 16){
+        console.log(`O total da soma é ${soma}, portanto é menor que 16`)
+    } else if(soma > 15){
+        console.log(soma)
+    }
+
+*/
+/*
+1.
+
+let n = 5
+
+let linhasDoAsterisco = "*";
+let linha = ""
+ 
+
+for(l = 0; l < n; l++){
+     linha += linhasDoAsterisco;
+    }
+    for(c = 0; c < l; c ++){
+        console.log(linha);    
+    }
+    
+
+
+
+2.
+
+let n = 5
+
+let linhasDoAsterisco = "";
+
+for(linha = 0; linha < n; linha ++){ 
+    linhasDoAsterisco += "*";
+    console.log(linhasDoAsterisco)
+}
+*/
+3.
+let n = 5
+
+let linhasDoAsterisco = ""; 
+
+for(linha = 5; linha > 0; linha --){
+    linhasDoAsterisco += "*";
+    } 
+    for(c = 0; c < linha; c + +){
+        console.log(linha+ "  ");    
+    } 
