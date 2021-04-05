f09dbee1e03a4b555c6f5ff4c7043326f6a478aa
Johnny-batata <you@example.com>
2021-04-05 15:11:24 -0300

    exercícios finalizados

---
+++ exercises/bloco_8/dia_3/ex0001.js
@@ -0,0 +1,11 @@
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+  fullNames.forEach((fullName) => console.log(fullName))
+  
+  console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
