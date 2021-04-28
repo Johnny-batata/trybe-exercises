ad74a71e8909268ec47c5d9d4c7635b4889fe963
Johnny-batata <you@example.com>
2021-04-28 13:57:44 -0300

    exercícios do dia

---
+++ exercises/bloco_10/dia_2/exercise/exercise/src/data.js
@@ -0,0 +1,103 @@
+const pokemons = [
+    {
+        id: 25,
+        name: "Pikachu",
+        type: 'Electric',
+        averageWeight: {
+            value: 6.0,
+            measurementUnit: "kg"
+        },
+        image: "https://e7.pngegg.com/pngimages/68/770/png-clipart-pikachu-sprite-squirtle-raichu-pikachu-sprite-game-balloon.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)"
+    },
+    {
+        id: 4,
+        name: "Charmander",
+        type: 'Fire',
+        averageWeight: {
+            value: 8.5,
+            measurementUnit: "kg"
+        },
+        image: "https://www.pngjoy.com/pngm/383/7081135_charmander-sprite-pokemon-fire-red-charmander-sprite-png.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)"
+    },
+    {
+        id: 10,
+        name: "Caterpie",
+        type: 'Bug',
+        averageWeight: {
+            value: 2.9,
+            measurementUnit: "kg"
+        },
+        image: "https://www.seekpng.com/png/detail/182-1829970_caterpie-pokemon-sprite-animation-gifs.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)"
+    },
+    {
+        id: 23,
+        name: "Ekans",
+        type: 'Poison',
+        averageWeight: {
+            value: 6.9,
+            measurementUnit: "kg"
+        },
+        image: "https://www.pngfind.com/pngs/m/619-6192054_ekans-ekans-pokemon-pixel-art-hd-png-download.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)"
+    },
+    {
+        id: 65,
+        name: "Alakazam",
+        type: 'Psychic',
+        averageWeight: {
+            value: 48.0,
+            measurementUnit: "kg"
+        },
+        image: "https://www.pngitem.com/pimgs/m/513-5136707_alakazam-gen-5-sprite-hd-png-download.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)"
+    },
+    {
+        id: 151,
+        name: "Mew",
+        type: 'Psychic',
+        averageWeight: {
+            value: 4.0,
+            measurementUnit: "kg"
+        },
+        image: "https://www.seekpng.com/png/detail/202-2022850_mew-sprite-png-pokemon-pixel-art-mew.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)"
+    },
+    {
+        id: 78,
+        name: "Rapidash",
+        type: 'Fire',
+        averageWeight: {
+            value: 95.0,
+            measurementUnit: "kg"
+        },
+        image: "https://e7.pngegg.com/pngimages/679/659/png-clipart-pokemon-platinum-pokemon-firered-and-leafgreen-pokemon-let-s-go-pikachu-and-let-s-go-eevee-pokemon-x-and-y-rapidash-others.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)"
+    },
+    {
+        id: 143,
+        name: "Snorlax",
+        type: 'Normal',
+        averageWeight: {
+            value: 460.0,
+            measurementUnit: "kg"
+        },
+        image: "https://prowiki.info/images/archive/5/54/20190625141303%21Snorlax_Front_Sprite.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)"
+    },
+    {
+        id: 148,
+        name: "Dragonair",
+        type: 'Dragon',
+        averageWeight: {
+            value: 16.5,
+            measurementUnit: "kg"
+        },
+        image: "https://img.pokemondb.net/sprites/x-y/normal/dragonair.png",
+        moreInfo: "https:bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)"
+    }
+  ];
+  
+  export default pokemons;
